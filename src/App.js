ee2d7f83611002be7226755e607fdd98d529bc0f
CelineSelke1 <celineselke@outlook.com>
2025-05-03 20:45:27 -0400

    merge dev

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import GameOfLife from './GameOfLife';
+import Admin from './Admin';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
